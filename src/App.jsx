e83a051d78ecb35b767fe7d0ed9a1729670b00fa
abdulrdeveloper <abdulrahman2040b@gmail.com>
2026-05-16 12:19:32 +0500

    Update index.html title and restructure App component; add game component and assets

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "./assets/vite.svg";
-import heroImg from "./assets/hero.png";
 import "./App.css";
+import Game from "./Tic Tac Toe/game";
+
 
 function App() {
+
   return (
     <>
-    <h1>Welcome to React</h1>
+    <Game />
     </>
   );
 }
